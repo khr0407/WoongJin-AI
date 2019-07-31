b18982306c90e727d3718ac7fcfffd534b4cd8f0
Hera Koo <97khr7111@gmail.com>
2019-07-31 14:27:36 +0900

    test

--- app/src/main/java/edu/skku/woongjin_ai/MainActivity.java
+++ app/src/main/java/edu/skku/woongjin_ai/MainActivity.java
@@ -1,13 +1,60 @@
 package edu.skku.woongjin_ai;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView mListView;
+    public DatabaseReference mPostReference;
+    ArrayList<String> quizList;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        mPostReference = FirebaseDatabase.getInstance().getReference();
+
+        quizList = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1);
+        mListView.setAdapter(adapter);
+
+        getFirebaseDatabaseQuizList();
+    }
+
+    private void getFirebaseDatabaseQuizList(){
+        final ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                quizList.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    String key = snapshot.getKey();
+                    Log.d("quiz key", key);
+                    quizList.add(key);
+                }
+                adapter.clear();
+                adapter.addAll(quizList);
+                adapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {            }
+        };
+        mPostReference.child("quiz_list").addValueEventListener(postListener);
     }
 }
