b5f252199667d31b050f6caf2020c52fecdbc000
Hera Koo <97khr7111@gmail.com>
2019-08-09 15:59:27 +0900

    마이페이지 초안

--- app/src/main/java/edu/skku/woongjin_ai/MainActivity.java
+++ app/src/main/java/edu/skku/woongjin_ai/MainActivity.java
@@ -1,15 +1,20 @@
 package edu.skku.woongjin_ai;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
