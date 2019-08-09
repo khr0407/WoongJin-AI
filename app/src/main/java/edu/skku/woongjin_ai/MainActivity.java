0d9f4b473c11714b4d476cc9706337dee5491fb7
sala0320 <sala0320@naver.com>
2019-08-09 19:57:03 +0900

    LikeList

--- app/src/main/java/edu/skku/woongjin_ai/MainActivity.java
+++ app/src/main/java/edu/skku/woongjin_ai/MainActivity.java
@@ -1,21 +1,14 @@
 package edu.skku.woongjin_ai;
 
-
-import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -30,12 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
     ListView mListView;
     public DatabaseReference mPostReference;
-    ArrayList<String> scriptList;
+    ArrayList<String> quizList;
     ArrayAdapter<String> adapter;
-    Intent intent, intentType;
+    Intent intent, intentType, intentMyPage;
     String id;
     String check = "";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         mListView = (ListView) findViewById(R.id.listView);
         Button buttonSelectType = (Button) findViewById(R.id.selectType);
+        Button buttonMyPage = (Button) findViewById(R.id.myPage);
 
         intent = getIntent();
         id = intent.getStringExtra("id");
@@ -52,11 +45,21 @@ public class MainActivity extends AppCompatActivity {
 
         mPostReference = FirebaseDatabase.getInstance().getReference();
 
-        scriptList = new ArrayList<String>();
+        quizList = new ArrayList<String>();
         adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1);
         mListView.setAdapter(adapter);
 
         getFirebaseDatabaseQuizList();
+
+        buttonMyPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentMyPage = new Intent(MainActivity.this, MyPageActivity.class);
+                intentMyPage.putExtra("id", id);
+                startActivity(intentMyPage);
+                finish();
+            }
+        });
 
         buttonSelectType.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,23 +82,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void getFirebaseDatabaseScriptList(){
-
+    private void getFirebaseDatabaseQuizList(){
         final ValueEventListener postListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                scriptList.clear();
+                quizList.clear();
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
                     String key = snapshot.getKey();
                     quizList.add(key);
                 }
                 adapter.clear();
-                adapter.addAll(scriptList);
+                adapter.addAll(quizList);
                 adapter.notifyDataSetChanged();
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {            }
         };
-        mPostReference.child("script_list").addValueEventListener(postListener);
+        mPostReference.child("quiz_list").addValueEventListener(postListener);
     }
 }
--- app/src/main/java/edu/skku/woongjin_ai/MyPageActivity.java
+++ app/src/main/java/edu/skku/woongjin_ai/MyPageActivity.java
@@ -1,14 +1,66 @@
 package edu.skku.woongjin_ai;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MyPageActivity extends AppCompatActivity {
+    Intent intent, intentAddFriend;
+    String id;
+    String userName, userCoin;
+    Button buttonFriendList, LikeList, QList, userLetter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        intent = getIntent();
+        id = intent.getStringExtra("id");
+
+        buttonFriendList = findViewById(R.id.friendList);
+        LikeList = findViewById(R.id.LikeList);
+        QList = findViewById(R.id.QList);
+        TextView userNameT = (TextView) findViewById(R.id.userName);
+
+        userNameT.setText("ID: " + id);
+
+
+
+        buttonFriendList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Fintent = new Intent(MyPageActivity.this, FriendActivity.class);
+                Fintent.putExtra("id", id);
+                startActivity(Fintent);
+                finish();
+            }
+        });
+
+        LikeList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Lintent = new Intent(MyPageActivity.this, LikeListActivity.class);
+                Lintent.putExtra("id", id);
+                startActivity(Lintent);
+                //finish();
+            }
+        });
+
+        QList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Qintent = new Intent(MyPageActivity.this, QListActivity.class);
+                Qintent.putExtra("id", id);
+                startActivity(Qintent);
+                finish();
+            }
+        });
     }
+
+
 }
