f2bc4ef2d0fff638f85b1f87b8855835ca89741f
hyomin <hmybjm95@naver.com>
2019-08-09 15:58:37 +0900

    jsoncommit

--- app/src/main/java/edu/skku/woongjin_ai/MainActivity.java
+++ app/src/main/java/edu/skku/woongjin_ai/MainActivity.java
@@ -9,7 +9,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,56 +21,109 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView mListView;
-    public DatabaseReference mPostReference;
     ArrayList<String> scriptList;
     ArrayAdapter<String> adapter;
-
+    private DatabaseReference mPostReference;
+    String ID, name = "", gender = "";
+    long age = 0;
+    String sort = "id";
+    EditText ageET, nameET, genderET, idET;
+    Button btn;
+    ListView listView;
+    ArrayList<String> data;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mListView = (ListView) findViewById(R.id.listView);
-
+        data = new ArrayList<String>();
+        ageET = (EditText) findViewById(R.id.ageet);
+        nameET = (EditText) findViewById(R.id.nameet);
+        genderET = (EditText) findViewById(R.id.genderet);
+        idET = (EditText) findViewById(R.id.idet);
+        btn = (Button) findViewById(R.id.button);
+        listView = (ListView) findViewById(R.id.datalist);
         mPostReference = FirebaseDatabase.getInstance().getReference();
-
-        scriptList = new ArrayList<String>();
-        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1);
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String scriptname_key = scriptList.get(position);
-                Intent intent_script = new Intent(MainActivity.this, ScriptActivity.class);
-                intent_script.putExtra("script name", scriptname_key);
-                startActivity(intent_script);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ID = idET.getText().toString();
+                name = nameET.getText().toString();
+                gender = genderET.getText().toString();
+                String aget = ageET.getText().toString();
+                if ((ID.length() * name.length() * gender.length() * aget.length()) == 0) {
+                    Toast.makeText(MainActivity.this, "Data is missing", Toast.LENGTH_SHORT).show();
+                } else {
+                    age = Long.parseLong(aget);
+                    postFirebaseDatabase(true);
+                    Intent intent1 = new Intent(MainActivity.this, ScriptActivity.class);
+                    startActivity(intent1);
+                }
             }
         });
-        getFirebaseDatabaseScriptList();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
+        getFirebaseDatabase();
     }
 
-    private void getFirebaseDatabaseScriptList(){
-
+    public void getFirebaseDatabase() {
         final ValueEventListener postListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                scriptList.clear();
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    String key = snapshot.getKey();
-                    Log.d("script key", key);
-                    scriptList.add(key);
+                Log.d("onDataChange", "Data is Updated");
+                data.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    String key = postSnapshot.getKey();
+                    FirebasePost_script get = postSnapshot.getValue(FirebasePost_script.class);
+                    String[] info = {get.id, get.name, String.valueOf(get.age), get.gender};
+                    String result = info[0] + " : " + info[1] + "(" + info[3] + ", " + info[2] + ")";
+                    data.add(result);
+                    Log.d("getFirebaseDatabase", "key: " + key);
+                    Log.d("getFirebaseDatabase", "info: " + info[0] + info[1] + info[2] + info[3]);
                 }
-                adapter.clear();
-                adapter.addAll(scriptList);
-                adapter.notifyDataSetChanged();
+                arrayAdapter.clear();
+                arrayAdapter.addAll(data);
+                arrayAdapter.notifyDataSetChanged();
             }
+
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {            }
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
         };
-        mPostReference.child("script_list").addValueEventListener(postListener);
+        mPostReference.child("id_list").addValueEventListener(postListener);
+    }
+
+
+    public void postFirebaseDatabase(boolean add) {
+        Map<String, Object> childUpdates = new HashMap<>();
+        Map<String, Object> postValues = null;
+        if (add) {
+            FirebasePost_script post = new FirebasePost_script(ID, name, age, gender);
+            postValues = post.toMap();
+        }
+        childUpdates.put("/id_list/" + ID, postValues);
+        mPostReference.updateChildren(childUpdates);
+        clearET();
+    }
+
+
+    public void clearET() {
+        genderET.setText("");
+        ageET.setText("");
+        nameET.setText("");
+        idET.setText("");
+        gender = "";
+        age = 0;
+        name = "";
+        ID = "";
     }
 }
+
--- app/src/main/java/edu/skku/woongjin_ai/ScriptActivity.java
+++ app/src/main/java/edu/skku/woongjin_ai/ScriptActivity.java
@@ -1,20 +1,12 @@
 package edu.skku.woongjin_ai;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Chronometer;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,68 +14,54 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class ScriptActivity extends AppCompatActivity {
-    String scriptname_key;
     private DatabaseReference mPostReference;
-    TextView content, time;
-    Button start, stop, reset;
+    ListView friend_list;
+    ArrayList<String> data;
+    ArrayAdapter<String> arrayAdapter;
+    String id_key;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_script);
 
+        friend_list = findViewById(R.id.friend_list);
 
-        Intent intent = getIntent();
-        scriptname_key = intent.getStringExtra("script name");
-        mPostReference = FirebaseDatabase.getInstance().getReference().child("script_list").child(scriptname_key);
-        content = findViewById(R.id.content);
-        time = findViewById(R.id.time);
-        start = findViewById(R.id.start);
-        stop = findViewById(R.id.stop);
-        reset = findViewById(R.id.reset);
-        final Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
+        data = new ArrayList<String>();
 
+        final Intent intent = getIntent();
+        id_key = intent.getStringExtra("id");
+
+        mPostReference = FirebaseDatabase.getInstance().getReference().child("user_list").child(id_key).child("friend");
+        arrayAdapter = new ArrayAdapter<String>(ScriptActivity.this, android.R.layout.simple_list_item_1);
+        friend_list.setAdapter(arrayAdapter);
         getFirebaseDatabase();
-
-        start.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                chronometer.start();
-            }
-        });
-
-        stop.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                chronometer.stop();
-            }
-        });
-
-        reset.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                chronometer.setBase(SystemClock.elapsedRealtime());
-            }
-        });
     }
 
     public void getFirebaseDatabase() {
-        try{
+        try {
             final ValueEventListener postListener = new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    FirebasePost_script get = dataSnapshot.getValue(FirebasePost_script.class);
-                    content.setText(get.text);
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        String key = snapshot.getKey();
+                        Log.d("friend key", key);
+                        //if (key.equals(id_key))
+                        data.add(key);
+                    }
+                    arrayAdapter.clear();
+                    arrayAdapter.addAll(data);
+                    arrayAdapter.notifyDataSetChanged();
                 }
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
                 }
             };
             mPostReference.addValueEventListener(postListener);
-        }catch(java.lang.NullPointerException e){
+        } catch (java.lang.NullPointerException e) {
         }
     }
 }
