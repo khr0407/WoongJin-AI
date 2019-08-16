ef9d4bbcbfb35f2f70280cffe3a3371ff484dfa7
Hera Koo <97khr7111@gmail.com>
2019-08-16 21:18:01 +0900

    지문 배경 초안

--- app/src/main/java/edu/skku/woongjin_ai/ReadScriptActivity.java
+++ app/src/main/java/edu/skku/woongjin_ai/ReadScriptActivity.java
@@ -1,10 +1,12 @@
 package edu.skku.woongjin_ai;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -12,14 +14,22 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.w3c.dom.Text;
+
+import java.io.File;
 
 public class ReadScriptActivity extends AppCompatActivity {
     public DatabaseReference mPostReference;
     Intent intent;
     String title;
     TextView textview_title, textview_script;
+    ImageView backgroundImage;
+    FirebaseStorage storage;
+    private StorageReference storageReference;
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +39,23 @@ public class ReadScriptActivity extends AppCompatActivity {
         intent = getIntent();
         textview_title = (TextView)findViewById(R.id.textview_title);
         textview_script = (TextView)findViewById(R.id.textview_script);
+        backgroundImage = (ImageView) findViewById(R.id.background);
 
         title = intent.getStringExtra("scriptnm");
 
         mPostReference = FirebaseDatabase.getInstance().getReference().child("script_list");
+
         textview_title.setText(title);
         getFirebaseDatabase();
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        StorageReference pathReference = storageReference.child("scripts_background/고양이.jpg");
+
+        //TODO storage에서 지문 image 다운 및 background에 적용
+
+
+
     }
 
     private void getFirebaseDatabase() {
