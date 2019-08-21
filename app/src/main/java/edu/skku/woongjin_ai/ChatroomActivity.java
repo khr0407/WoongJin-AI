aadb9cedd0171e26209e61e790df1623e06be9cb
abc <seftware99@gmail.com>
2019-08-21 11:14:44 +0900

    popup

---
+++ app/src/main/java/edu/skku/woongjin_ai/ChatroomActivity.java
@@ -0,0 +1,63 @@
+package edu.skku.woongjin_ai;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ChatroomActivity extends Activity {
+    TextView txtText;
+    Intent intent;
+
+    ArrayList<String> data;
+    String roomname;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_chatroom);
+
+        data = new ArrayList<String>();
+        intent = getIntent();
+
+        roomname=intent.getStringExtra("room_name");
+
+
+    }
+
+    //확인 버튼 클릭
+    public void mOnClose(View v){
+        //데이터 전달하기
+        Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(RESULT_OK, intent);
+
+        //액티비티(팝업) 닫기
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        //안드로이드 백버튼 막기
+//        return;
+//    }
+}
