f5e0a142a4bf6983ba7e895ee6b45aaa02878adc
Hera Koo <97khr7111@gmail.com>
2019-08-26 12:29:24 +0900

    카메라 초벌

--- app/src/main/java/edu/skku/woongjin_ai/HintVideoFragment.java
+++ app/src/main/java/edu/skku/woongjin_ai/HintVideoFragment.java
@@ -1,6 +1,9 @@
 package edu.skku.woongjin_ai;
 
 import android.content.Context;
+import android.graphics.Camera;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,12 +11,17 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
-public class HintVideoFragment extends Fragment {
+import java.io.IOException;
+
+public class HintVideoFragment extends Fragment implements SurfaceHolder.Callback {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -21,7 +29,14 @@ public class HintVideoFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    String type;
+    String type, path;
+    Camera camera;
+    boolean isRecording = false, isPlaying = false, hasVideo = false;
+    MediaRecorder mediaRecorder = null;
+    MediaPlayer mediaPlayer = null;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder = null;
+    ImageButton recordButton, recordAgainButton;
 
     private HintVideoFragment.OnFragmentInteractionListener mListener;
 
@@ -55,13 +70,66 @@ public class HintVideoFragment extends Fragment {
 
         type = getArguments().getString("type");
 
-        ImageButton recordButton = (ImageButton) view.findViewById(R.id.record);
+        recordButton = (ImageButton) view.findViewById(R.id.record);
+        recordAgainButton = (ImageButton) view.findViewById(R.id.watchVideo);
         ImageButton goBackButton = (ImageButton) view.findViewById(R.id.goBack);
+        mediaRecorder = new MediaRecorder();
+        mediaPlayer = new MediaPlayer();
+        surfaceView = (SurfaceView) view.findViewById(R.id.sv);
+        camera = new Camera();
 
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                hasVideo = false;
+                if(isRecording == false) {
+                    initAudioRecorder();
+                    mediaRecorder.start();
+                    isRecording = true;
+                    recordButton.setImageResource(R.drawable.ic_icons_recording_video);
+                    Log.d("hereeeeeeeeee", "stop recording");
+                } else {
+                    mediaRecorder.stop();
+                    isRecording = false;
+                    recordButton.setImageResource(R.drawable.ic_icons_take_video);
+                    Log.d("hereeeeeeeeee", "stop recording");
+                }
+                hasVideo = true;
+                initVideoRecorder();
+                startVideoRecorder();
+            }
+        });
 
+        recordAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPlaying == false) {
+                    try {
+                        mediaPlayer.setDataSource(path);
+                        if(hasVideo == true) {
+                            mediaPlayer.setDisplay(surfaceHolder);
+                            mediaPlayer.setOnCompletionListener(myListener);
+                        }
+                        mediaPlayer.prepare();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    mediaPlayer.start();
+                    isPlaying = true;
+                    Log.d("hereeeeeeeeee", "stop playing");
+                } else {
+                    mediaPlayer.stop();
+                    isPlaying = false;
+                    Log.d("hereeeeeeeeee", "start playing1");
+                }
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                isPlaying = false;
+                Log.d("hereeeeeeeeee", "start playing2");
             }
         });
 
@@ -86,6 +154,26 @@ public class HintVideoFragment extends Fragment {
         return view;
     }
 
+    MediaPlayer.OnCompletionListener myListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            Log.d("hereeeeeeeee", "start playing3");
+        }
+    };
+
+    void initVideoRecorder() {
+//        camera = Camera.open();
+
+    }
+
+    void initAudioRecorder() {
+
+    }
+
+    void startVideoRecorder() {
+
+    }
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -101,6 +189,21 @@ public class HintVideoFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 
     public interface OnFragmentInteractionListener {
