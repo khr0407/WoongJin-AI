4049945463265094e18e841c19fdd346828ddd92
Hera Koo <97khr7111@gmail.com>
2019-08-27 16:32:50 +0900

    친구문제 재벌 (랜덤 아님)

--- app/src/main/java/edu/skku/woongjin_ai/ShowFriendQuizActivity.java
+++ app/src/main/java/edu/skku/woongjin_ai/ShowFriendQuizActivity.java
@@ -1,10 +1,13 @@
 package edu.skku.woongjin_ai;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,6 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ShowFriendQuizActivity extends AppCompatActivity {
 
@@ -25,8 +29,10 @@ public class ShowFriendQuizActivity extends AppCompatActivity {
     String id, scriptnm, background;
     public DatabaseReference mPostReference;
     ListView myFriendQuizListView, likeQuizListView;
-    ArrayList<String> myFriendQuizList, likeQuizList, myFriendList;
-    ArrayAdapter<String> myFriendQuizAdapter, likeQuizAdapter;
+    ArrayList<String> likeQuizList, myFriendList;
+    ArrayList<QuizOXShortwordTypeInfo> myFriendOXQuiz, myFriendShortQuizList;
+    ArrayList<QuizChoiceTypeInfo> myFriendChoiceQuizList;
+    MyFriendQuizListAdapter myFriendQuizListAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,11 +50,12 @@ public class ShowFriendQuizActivity extends AppCompatActivity {
         likeQuizListView = (ListView) findViewById(R.id.likeQuizList);
         TextView textView = (TextView) findViewById(R.id.textShowFriendQuiz);
 
-        myFriendQuizList = new ArrayList<String>();
         likeQuizList = new ArrayList<String>();
         myFriendList = new ArrayList<String>();
-        myFriendQuizAdapter = new ArrayAdapter<String>(ShowFriendQuizActivity.this, android.R.layout.simple_list_item_1);
-        likeQuizAdapter = new ArrayAdapter<String>(ShowFriendQuizActivity.this, android.R.layout.simple_list_item_1);
+        myFriendOXQuiz = new ArrayList<QuizOXShortwordTypeInfo>();
+        myFriendShortQuizList = new ArrayList<QuizOXShortwordTypeInfo>();
+        myFriendChoiceQuizList = new ArrayList<QuizChoiceTypeInfo>();
+        myFriendQuizListAdapter = new MyFriendQuizListAdapter();
 
         getFirebaseDatabaseMyFriendQuiz();
         getFirebaseDatabaseLikeQuiz();
@@ -60,6 +67,76 @@ public class ShowFriendQuizActivity extends AppCompatActivity {
         mPostReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                myFriendList.clear();
+                myFriendOXQuiz.clear();
+                myFriendShortQuizList.clear();
+                myFriendChoiceQuizList.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String key = snapshot.getKey();
+                    if(key.equals("kakaouser_list") || key.equals("user_list")) {
+                        for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            String key1 = snapshot1.getKey();
+                            if(key1.equals(id)) {
+                                for(DataSnapshot snapshot2 : snapshot1.child("friend").getChildren()) {
+                                    String key2 = snapshot2.getKey();
+                                    myFriendList.add(key2);
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                for(DataSnapshot snapshot : dataSnapshot.child("quiz_list/" + scriptnm).getChildren()) {
+                    String key = snapshot.getKey();
+                    if(key.equals("type1") || key.equals("type3")) {
+                        for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            String uid = snapshot1.child("uid").getValue().toString();
+                            for(String friend : myFriendList) {
+                                if(uid.equals(friend)) {
+                                    QuizOXShortwordTypeInfo quiz = snapshot1.getValue(QuizOXShortwordTypeInfo.class);
+                                    myFriendOXQuiz.add(quiz);
+                                    break;
+                                }
+                            }
+                        }
+                    } else if(key.equals("type2")) {
+                        for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            String uid = snapshot1.child("uid").getValue().toString();
+                            for(String friend : myFriendList) {
+                                if(uid.equals(friend)) {
+                                    QuizChoiceTypeInfo quiz = snapshot1.getValue(QuizChoiceTypeInfo.class);
+                                    myFriendChoiceQuizList.add(quiz);
+                                    break;
+                                }
+                            }
+                        }
+                    } else if(key.equals("type3")) {
+                        for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            String uid = snapshot1.child("uid").getValue().toString();
+                            for(String friend : myFriendList) {
+                                if(uid.equals(friend)) {
+                                    QuizOXShortwordTypeInfo quiz = snapshot1.getValue(QuizOXShortwordTypeInfo.class);
+                                    myFriendShortQuizList.add(quiz);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+//                Random generator = new Random();
+
+                for(QuizOXShortwordTypeInfo quiz : myFriendOXQuiz) {
+                    myFriendQuizListAdapter.addItem(quiz.question, quiz.uid, ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_icons_question_mark), quiz.like);
+                }
+                for(QuizChoiceTypeInfo quiz : myFriendChoiceQuizList) {
+                    myFriendQuizListAdapter.addItem(quiz.question, quiz.uid, ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_icons_question_mark), quiz.like);
+                }
+                for(QuizOXShortwordTypeInfo quiz : myFriendShortQuizList) {
+                    myFriendQuizListAdapter.addItem(quiz.question, quiz.uid, ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_icons_question_mark), quiz.like);
+                }
+                myFriendQuizListView.setAdapter(myFriendQuizListAdapter);
 
             }
             @Override
