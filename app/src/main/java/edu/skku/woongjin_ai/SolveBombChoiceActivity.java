bc00395e1f6a52aff6be29a95b02735bdfcb4d76
doyoon <32251814+doyoon0307@users.noreply.github.com>
2019-11-05 01:55:59 +0900

    Update SolveBombChoiceActivity.java

--- app/src/main/java/edu/skku/woongjin_ai/SolveBombChoiceActivity.java
+++ app/src/main/java/edu/skku/woongjin_ai/SolveBombChoiceActivity.java
@@ -280,4 +280,8 @@ public class SolveBombChoiceActivity extends AppCompatActivity implements ShowSc
     public void onFragmentInteraction(Uri uri) {
 
     }
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+    }
 }
